3c94a3f6d2dd51531308841e0436f0b1badb6994
Jake Mazzone <mazzone.15@osu.edu>
2019-03-11 13:18:57 -0400

    Lab 6

--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { BooksComponent } from './books/books/books.component';
+import { BokRowComponent } from './books/bok-row/bok-row.component';
+import { BookRowComponent } from './books/book-row/book-row.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    BooksComponent,
+    BokRowComponent,
+    BookRowComponent
   ],
   imports: [
     BrowserModule,
